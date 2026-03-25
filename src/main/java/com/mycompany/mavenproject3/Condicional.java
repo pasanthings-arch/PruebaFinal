bdfec76b71e27c72a2b1e5514c172d96321973ac
pasanthings-arch <promocionoropa@gmail.com>
2026-03-25 13:06:46 +0100

    Programa de condicional

---
+++ src/main/java/com/mycompany/mavenproject3/Condicional.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author Sinner
+ */
+public class Condicional {
+    public static void main(String[] args) {
+        
+        int edad=12;
+        int mayoriaEdad=18;
+        
+        if(edad>=mayoriaEdad){
+            System.out.println("Tiene mayoria de edad"+edad);
+        }else{
+            System.out.println("No tiene mayoria de eda"+edad);
+        }
+    }
+    
+    
+}
